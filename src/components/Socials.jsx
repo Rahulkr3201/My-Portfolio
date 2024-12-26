b123eddcb553d506188939fb7bd4fa8b89165b65
Rahul Kumar <172767765+Rahulkr3201@users.noreply.github.com>
2024-12-26 16:44:50 +0530

    Update Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -52,7 +52,7 @@ function Socials() {
   ];
 
   return (
-    <motion.div className="mt-2 mb-3">
+    <div className="mt-2 mb-3">
       <Heading>Get in Touch</Heading>
       <div className="m-4 ml-0">
         <ul
@@ -80,7 +80,7 @@ function Socials() {
           ))}
         </ul>
       </div>
-    </motion.div>
+    </div>
   );
 }
 
